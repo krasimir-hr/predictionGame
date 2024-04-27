794ed15e9e084b90077f2b288be48b3b33d8699c
krasimir-hr <krasimir.hr@icloud.com>
2024-04-27 15:08:10 +0300

    again some updates

--- predictionGame/settings.py
+++ predictionGame/settings.py
@@ -102,7 +102,6 @@ DATABASES = {
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse(database_url)
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- predictionGame/tournament/models.py
+++ predictionGame/tournament/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=20)
-    logo = models.ImageField(upload_to='team_logos/')
+    logo = models.URLField()
 
     def __str__(self):
         return f"{self.name}"
