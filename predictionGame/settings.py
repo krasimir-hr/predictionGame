c1db77c511352f4d1d41c2cc071e2028d934fb41
krasimir-hr <krasimir.hr@icloud.com>
2024-04-27 12:30:54 +0300

    settings.py updated

--- predictionGame/settings.py
+++ predictionGame/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
