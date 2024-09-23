feb88a5585aa796fcde73c771e3da046322403ac
krasimir-hr <krasimir.hr@icloud.com>
2024-09-24 01:20:59 +0300

    update 2.0

--- predictionGame/settings.py
+++ predictionGame/settings.py
@@ -103,13 +103,13 @@ DATABASES = {
     }
 }
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # database_url = os.environ.get("DATABASE_URL")
 # DATABASES["default"] = dj_database_url.parse(database_url)
