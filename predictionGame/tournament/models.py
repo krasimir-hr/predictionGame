c79f2290d78ccb196378fee06d8e25d7a07be4ac
krasimir-hr <krasimir.hr@icloud.com>
2024-09-24 02:01:27 +0300

    update 2.0

--- predictionGame/tournament/models.py
+++ predictionGame/tournament/models.py
@@ -6,7 +6,7 @@ import json
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     display_name = models.CharField(max_length=50, null=True, blank=True)
     logo = models.URLField(null=True, blank=True)
 
